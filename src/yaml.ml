b5762c3ba626178b46d9c81df6d8121ad2e8935e
mwipliez <mwipliez@9e8d6642-7d5e-4fb4-b084-ba3e547af5c7>
2009-04-16 15:53:30 +0000

    First implementation of using plain-style scalars.
    git-svn-id: svn://forge.ocamlcore.org/svn/yaml4caml@7 9e8d6642-7d5e-4fb4-b084-ba3e547af5c7

--- src/yaml.ml
+++ src/yaml.ml
@@ -76,17 +76,56 @@ let utf8 array =
 (* Unicode character lexers *)
 
 let utf8_len = [|        (* Char byte length according to first UTF-8 byte. *)
-  1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 
-  1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 
-  1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 
-  1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 
-  1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 
-  1; 1; 1; 1; 1; 1; 1; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 
-  0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 
-  0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 
-  0; 0; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 
-  2; 2; 2; 2; 2; 2; 2; 2; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 
-  4; 4; 4; 4; 4; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 |]
+	1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; (* from 0 *)
+	1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1;
+	1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1;
+	1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1;
+	1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1;
+	1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1;
+	1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1;
+	1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; 1; (* to 127 *)
+	0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0;
+	0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0;
+	0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0;
+	0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0;
+	0; 0; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; (* from 194 to 223 *)
+	2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2; 2;
+	3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; (* from 224 to 239 *)
+	4; 4; 4; 4; 4; 4; 4; 4; 0; 0; 0; 0; 0; 0; 0; 0 |] (* from 240 to 247 *)
+
+let uchar_utf8 i =
+  let b0 = i () in
+  begin match utf8_len.(b0) with
+  | 0 -> raise Malformed
+  | 1 -> b0
+  | 2 ->
+      let b1 = i () in
+      if b1 lsr 6 != 0b10 then raise Malformed else
+      ((b0 land 0x1F) lsl 6) lor (b1 land 0x3F)
+  | 3 ->
+      let b1 = i () in
+      let b2 = i () in
+      if b2 lsr 6 != 0b10 then raise Malformed else
+      begin match b0 with
+      | 0xE0 -> if b1 < 0xA0 || 0xBF < b1 then raise Malformed else ()
+      | 0xED -> if b1 < 0x80 || 0x9F < b1 then raise Malformed else ()
+      | _ -> if b1 lsr 6 != 0b10 then raise Malformed else ()
+      end;
+      ((b0 land 0x0F) lsl 12) lor ((b1 land 0x3F) lsl 6) lor (b2 land 0x3F)
+  | 4 -> 
+      let b1 = i () in
+      let b2 = i () in
+      let b3 = i () in
+      if  b3 lsr 6 != 0b10 || b2 lsr 6 != 0b10 then raise Malformed else
+      begin match b0 with
+      | 0xF0 -> if b1 < 0x90 || 0xBF < b1 then raise Malformed else ()
+      | 0xF4 -> if b1 < 0x80 || 0x8F < b1 then raise Malformed else ()
+      | _ -> if b1 lsr 6 != 0b10 then raise Malformed else ()
+      end;
+      ((b0 land 0x07) lsl 18) lor ((b1 land 0x3F) lsl 12) lor 
+      ((b2 land 0x3F) lsl 6) lor (b3 land 0x3F)
+  | _ -> assert false
+  end
 
 (*****************************************************************************)
 
@@ -186,18 +225,131 @@ let mapStyle map =
 	else
 		Block
 
+(*****************************************************************************)
 (* the buffer will be used by every output. *)
 let b = Buffer.create 16384
 let outc c = Buffer.add_char b c
 let outs s = Buffer.add_string b s
 let num_sp = 2
 
+let ppSingleQuotes str =
+	outc '\'';
+	outs str;
+	outc '\''
+
+let ppDoubleQuotes str =
+	outc '"';
+	outs str;
+	outc '"'
+
+let ppNoQuotes str = outs str
+
+(** [mustUseSingle uchar] is true whenever it would not be possible to print 
+a string containing [uchar] using the plain style, but it would be possible
+with single quote style. Rules enforced are no ": " sequence, no " #" sequence,
+no ',', no '[', no ']', no '{', and no '}'. *)
+let mustUseSingle =
+	(* last char read, initially 0 *)
+	let lastChar = ref 0 in
+	function uchar ->
+		let res =
+			(* Plain scalars must never contain the ": " and " #" character combinations *)
+			!lastChar = 0x3A (* ':' *) && uchar = 0x20 ||
+			!lastChar = 0x20 && uchar = 0x23 (* '#' *) ||
+			(* In addition, inside flow collections, plain scalars must not contain *)
+			(* the '[', ']', '{', '}' and ',' characters. *)
+			(* Note: we further restrict plain scalars by applying this regardless of *)
+			(* whether the scalar is in a flow collection. *)
+			uchar = 0x2C (* ',' *) ||
+			uchar = 0x5B || uchar = 0x5D || (* '[', ']' *)
+			uchar = 0x7B || uchar = 0x7D (* '{', '}' *)
+		in
+		lastChar := uchar;
+		res
+
+(** [mustUseDouble uchar] is true whenever it would not be possible to print 
+a string containing [uchar] using the plain style nor the single quote style,
+but it would be possible with double quote style.
+Rules enforced are no non-printable characters in the string (as required by
+the standard), as well as no line breaks. *)
+let mustUseDouble uchar =
+	(* C0 block, except TAB 0x09 *)
+	(* We consider that LF 0x0A and CR 0x0D require double-quoting style, for *)
+	(* the sake of simplicity. *)
+	uchar >= 0 && uchar < 0x09 || uchar > 0x09 && uchar < 0x20 ||
+	
+	(* DEL, C1 block, except NEL 0x85 *)
+	uchar >= 0x7F && uchar <= 0x9F && uchar <> 0x85 ||
+	
+	(* surrogate block 0xD800-0xDFFF, and 0xFFFE and 0xFFFF *)
+	uchar >= 0xD800 && uchar <= 0xDFFF || uchar = 0xFFFE || uchar = 0xFFFF
+
+exception Done
+
 (* this function prints the given string using double-quoted, *)
 (* single-quoted, or plain style according to the characters *)
 (* the string contains. *)
-let ppString s =
+let ppString str =
+	let i = ref 0 in
+	let length = String.length str in
 	
-	outc '"'; outs s; outc '"'
+	(* read_char is used by uchar_utf8 to read UTF-8 bytes from [str] and *)
+	(* return a Unicode char (as an int). *)
+	let read_char () =
+		if !i = length then
+			raise Malformed
+		else
+			let char = String.unsafe_get str !i in
+			incr i;
+			int_of_char char
+	in
+
+	try
+		(* a plain scalar must not contain leading or trailing white space characters. *)
+		
+		
+		(* for each Unicode char in str, check whether it would be necessary to *)
+		(* use single quote style or double quote style. *)
+		(* Note that we do not use String.iter because we deal with UTF-8 here. *)
+		while !i < length do
+			let uchar = uchar_utf8 read_char in
+			if mustUseSingle uchar then (
+				ppSingleQuotes str;
+				raise Done
+			);
+
+			if mustUseDouble uchar then (
+				ppDoubleQuotes str;
+				raise Done
+			)
+		done;
+		
+		if length = 0 then (
+			(* a plain scalar must not be empty *)
+			ppSingleQuotes str
+		) else if length = 1 then (
+			i := 0;
+			let s0 = uchar_utf8 read_char in
+			match s0 with
+				| 0x2D | 0x3A | 0x3F -> ppSingleQuotes str
+				| _ -> ppNoQuotes str
+		) else (
+			(* check whether the two first chars allow the use of plain style *)
+
+			(* the ':', '?' and '-' indicators may be used as the first character *)
+			(* if followed by a non-space character, as this causes no ambiguity. *)
+			i := 0;
+			let s0 = uchar_utf8 read_char in
+			let s1 = uchar_utf8 read_char in
+			match (s0, s1) with
+				| (0x2D, 0x09) | (0x2D, 0x20) (* '-' = 0x2D *)
+				| (0x3A, 0x09) | (0x3A, 0x20) (* ':' = 0x3A *)
+				| (0x3F, 0x09) | (0x3F, 0x20) (* '?' = 0x3F *) -> ppSingleQuotes str
+				| _ ->
+					(* nothing prevents us from writing a plain style scalar *)
+					ppNoQuotes str
+		)
+	with Done -> ()
 
 let ppScalar = function
 	| Binary bin -> outc '"'; outs bin; outc '"'
